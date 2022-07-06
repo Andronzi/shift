a1c3e1118c4fc95c812cd27f564dfeeee53135f4
Andronzi <andrey.radioniv.2003@gmail.com>
2022-07-06 14:17:00 +0500

    add react-hook-doom and date validation prev cent

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,37 +1,49 @@
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import '../scss/form.scss';
 
+type Inputs = {
+    firstName: string,
+    lastName: string,
+    paterName: string,
+    bornDate: Date 
+}
+
 export default function Form() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+    const date = new Date();
+    const dateData = [(date.getFullYear() - 18).toString()[3], date.getMonth() + 1, date.getDay()];
+
+    let regex = new RegExp(`^(19[0-9]{2}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])|(200[0-${dateData[0]}]-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])))`, 'i');
+
     return (
-        <div>
-            <form onSubmit={() => {}}>
-                <label>
-                    <span>
-                        Имя получателя
-                    </span>
-                    <input type="text" />
-                </label>
-                <label>
-                    <span>
-                        Фамилия получателя
-                    </span>
-                    <input type="text" />
-                </label>
-                <label>
-                    <span>
-                        Отчество получателя
-                    </span>
-                    <input type="text" />
-                </label>
-                <label>
-                    <span>
-                        Дата рождения получателя
-                    </span>
-                    <input type="date" />
-                </label>
-                <button type='submit'>
-                    Отправить
-                </button>
-            </form>
-        </div>
+        <><form onSubmit={handleSubmit(onSubmit)}>
+            <label>
+                <span>
+                    Имя получателя
+                </span>
+                <input {...register("firstName", { required: true, min: 2, max: 30 })} />
+            </label>
+            <label>
+                <span>
+                    Фамилия получателя
+                </span>
+                <input  {...register("lastName", { required: true, min: 2, max: 30 })} />
+            </label>
+            <label>
+                <span>
+                    Отчество получателя
+                </span>
+                <input {...register("paterName", { min: 2, max: 30 })} />
+            </label>
+            <label>
+                <span>
+                    Дата рождения получателя
+                </span>
+                <input type="date" {...register("bornDate", { pattern: regex })} />
+            </label>
+            <input value="отправить" type="submit" />
+        </form></>
     )
 }

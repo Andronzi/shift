482ee7f23c452073ff92f31a3d6c7832f28d39dd
Andronzi <andrey.radioniv.2003@gmail.com>
2022-07-12 15:25:59 +0400

    try to setting paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Form from "./Components/Form";
+import Form from "@components/Form/Form";
 
 function App() {
   return (
---
+++ src/Components/Form/Form.tsx
@@ -0,0 +1,26 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { validateDate } from '@utils/helpers/validation.helper';
+import './form.scss';
+
+type Inputs = {
+    firstName: string,
+    lastName: string,
+    paterName: string,
+    bornDate: Date 
+}
+
+export default function Form() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+    const dateValue: Date = watch("bornDate");
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input className="special" placeholder='Имя получателя' {...register("firstName", { required: true, min: 2, max: 30 })} />
+            <input placeholder='Фамилия получателя'  {...register("lastName", { required: true, min: 2, max: 30 })} />
+            <input placeholder='Отчество получателя' {...register("paterName", { min: 2, max: 30 })} />
+            <input placeholder='Дата рождения получателя' type="date" {...register("bornDate", { pattern: validateDate(dateValue) })} />
+            <input value="отправить" type="submit" />
+        </form>
+    )
+}
